2d1d993f11b705f485085d1810ee2ae50d08164d
unlomtrois <vadim.kuz02@gmail.com>
2021-12-13 01:42:52 +0300

    styles for searchbar

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -3,7 +3,7 @@ import styles from "./SearchBar.module.css";
 const SearchBar = () => {
   return (
     <div className={styles.wrapper}>
-      <input type="search" name="book-search" id="searchbar" className={styles.search_bar} />
+      <input type="search" name="book-search" id="searchbar" className={styles.search_bar} placeholder="Типа поиск" />
     </div>
   );
 };
