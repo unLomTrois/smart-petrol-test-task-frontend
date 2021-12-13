e05abbbd912c44f3624212ae7a128732982c35a0
unlomtrois <vadim.kuz02@gmail.com>
2021-12-13 15:51:52 +0300

    add lk button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,10 +4,17 @@ import styles from "./Header.module.css";
 const Header = () => {
   return (
     <div className={styles.wrapper}>
-      <SearchBar />
-      <h1 className={styles.title}>
-        <a href="/" className={styles.link}>Типа библиотека</a>
-      </h1>
+      <div className={styles.left_part}>
+        <SearchBar />
+        <h1 className={styles.title}>
+          <a href="/" className={styles.link}>
+            Типа библиотека
+          </a>
+        </h1>
+      </div>
+      <a href="/me" className={styles.lk}>
+        Личный кабинет
+      </a>
     </div>
   );
 };
