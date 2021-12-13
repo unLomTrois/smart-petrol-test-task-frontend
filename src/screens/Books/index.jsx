3a41a037d2853f6cd9a6bcfe0dc945bf4dd05f91
unlomtrois <vadim.kuz02@gmail.com>
2021-12-13 15:33:25 +0300

    change book item structure

--- src/screens/Books/index.jsx
+++ src/screens/Books/index.jsx
@@ -6,18 +6,21 @@ import Header from "components/Header";
 const SingleBook = ({ title, author, pages }) => {
   return (
     <div className={styles.book_item__wrapper}>
-      <h2 className={styles.book_item__title}>{title}</h2>
-      <p className={styles.book_item__author}>{author}</p>
-      <br />
-      <p name="description" className={styles.book_item__description}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
-        mollitia velit dicta deserunt pariatur dolorem enim labore maxime dolor
-        aperiam animi natus temporibus vero a. Consectetur commodi, impedit
-        alias assumenda necessitatibus rerum? Nisi labore culpa quaerat saepe
-        reprehenderit necessitatibus omnis perferendis. Officia dolore nihil
-        nisi aperiam fugit maxime autem veritatis.
-      </p>
-      {/* <p className={styles.book_item__pages}>{pages}</p> */}
+      <div className={styles.book_item__header}>
+        <h2 className={styles.book_item__title}>{title}</h2>
+        <p className={styles.book_item__author}>{author}</p>
+      </div>
+      <div className={styles.book_item__body}>
+        <p className={styles.book_item__description}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus,
+          mollitia velit dicta deserunt pariatur dolorem enim labore maxime
+          dolor aperiam animi natus temporibus vero a. Consectetur commodi,
+          impedit alias assumenda necessitatibus rerum? Nisi labore culpa
+          quaerat saepe reprehenderit necessitatibus omnis perferendis. Officia
+          dolore nihil nisi aperiam fugit maxime autem veritatis.
+        </p>
+        {/* <p className={styles.book_item__pages}>{pages}</p> */}
+      </div>
     </div>
   );
 };
